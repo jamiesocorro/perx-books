0c6b2f7d240acc130235a4198cc9d61305c6a344
Jamie Socorro <jamie_socorro@live.com>
2023-12-30 18:04:55 +0800

    feat: added custom text component

--- src/app/modules/custom/custom.component.ts
+++ src/app/modules/custom/custom.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from "@angular/core";
 import { NavigationBarComponent } from "src/app/shared/components/navigation-bar/navigation-bar.component";
-
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
 @Component({
   selector: "app-custom",
   templateUrl: "./custom.component.html",
   styleUrls: ["./custom.component.scss"],
-  imports: [NavigationBarComponent],
+  imports: [CommonModule, NavigationBarComponent, MatButtonModule],
+
   standalone: true,
 })
 export class CustomComponent implements OnInit {
-  constructor() {}
+  public title: string = "Default text";
+  public isUpdated: boolean = false;
+  public constructor() {}
 
-  ngOnInit(): void {}
+  public ngOnInit(): void {}
+
+  public updateTitle() {
+    this.isUpdated = true;
+  }
 }
